a92da9b5a8a9f740b744ed4936db7d73778de6b2
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-18 17:15:00 -0300

    STEP_02 - Wit Ai Initial Setup

--- src/answers/farewells.js
+++
@@ -1,6 +0,0 @@
-exports.farewells = [
-  'Tchau!',
-  'Ate mais',
-  'Tenha um excelente dia!',
-  'Até a próxima',
-];
--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,6 @@
-const { answer } = require('./entities');
 const { parse } = require('./wit');
 
 exports.answer = async message => {
   const parsedMessage = await parse(message);
-  return answer(parsedMessage);
+  return JSON.stringify(parsedMessage);
 };
