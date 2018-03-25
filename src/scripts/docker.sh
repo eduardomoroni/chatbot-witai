225e112e24361f7be10ea89a6e1eb7c7c0d59cf2
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-25 13:13:41 -0300

    build: fix Dockerfile to run hubot on Docker

--- src/scripts/docker.sh
+++ src/scripts/docker.sh
@@ -7,8 +7,9 @@ docker build \
   -f Dockerfile .
 
 docker run \
-  -e "NODE_ENV=production" \
+  -it \
+  -v /$(pwd):/usr/projects/chatbot-witai \
   --env-file ".env" \
-  -m "500M" --memory-swap "1G" \
-  -p "3000:3000" \
+  -m "500M" \
+  --memory-swap "500M" \
   chatbot-witai
